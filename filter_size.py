0dd5a136ea05080ce226d962dad56653696cbfbe
Rod C <cayabyabrr@yahoo.com>
2026-04-13 18:01:48 -0700

    filter word count

--- filter_size.py
+++ filter_size.py
@@ -8,11 +8,19 @@ def main():
     pass
 
 def filter_size_small():
-    with jsonlines.open("./data/data_copy.jsonl", "r") as f:
-        for i, line in enumerate(islice(f, 1, 201), start=1):
+    with jsonlines.open("./data/data_copy.jsonl", "r") as reader, \
+        jsonlines.open("./data/filtered.jsonl", "w") as writer:
+        for i, line in enumerate(islice(reader, 1, 201), start=1):
             prompt = line["conversations"][0]["content"]
             print(prompt, f"- (index = {i})")
             print("Word count -", word_count(prompt))
+            if word_count(prompt) > 100:
+                writer.write(line)
+
+
+    # with open("filtered.jsonl", "x") as f:
+        
+        
 
 def word_count(x):
     return len(list(x))
