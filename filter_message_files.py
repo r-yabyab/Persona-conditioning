ced2577a2b3d1349a877afc656f612e0ce82512f
Rod C <cayabyabrr@yahoo.com>
2026-04-19 18:35:34 -0700

    list hmtl files

--- filter_message_files.py
+++ filter_message_files.py
@@ -4,23 +4,24 @@ import json
 root = "./data/messages"
 
 def main():
-    for file in os.listdir(root):
-        folder_path = os.path.join(root, file)
+    for folder in os.listdir(root):
+        folder_path = os.path.join(root, folder)
 
         if os.path.isdir(folder_path):
-            for file in os.listdir(folder_path):
-                if file.endswith(".json"):
-                    print(file, folder_path)
+            file_path = os.path.join(folder_path, "messages.json")
+            print(count_json_lines(file_path), f" {file_path}", sep="")
 
 def count_words():
     with open(r"data\messages\c249294030477852672\messages.json", "r") as reader:
         for line in json.load(reader):
             print(line["Contents"])
 
-def count_json_lines():
-    with open(r"data\messages\c249294030477852672\messages.json", "r") as reader:
-        print(len(json.load(reader)))
+def count_json_lines(x):
+    # with open(r"data\messages\c249294030477852672\messages.json", "r") as reader:
+    #     print(len(json.load(reader)))
+    with open(x, "r", encoding="utf-8") as reader:
+        return len(json.load(reader))
 
-# main()
+main()
 # count_words()
-count_json_lines()
+# count_json_lines()
---
+++ html_parser.py
@@ -0,0 +1,9 @@
+import os
+import json
+
+root = "./data/localex/msg/dms"
+
+def main():
+    print(len(os.listdir(root)))
+
+main()
