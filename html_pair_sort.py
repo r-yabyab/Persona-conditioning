d4e9b44219df2cf2f23e13c9229560ec6610721b
Rod C <cayabyabrr@yahoo.com>
2026-04-21 20:09:09 -0700

    score sort

--- html_pair_sort.py
+++ html_pair_sort.py
@@ -1,4 +1,5 @@
 import jsonlines
+import json
 
 def main():
 
@@ -12,16 +13,25 @@ def main():
     max_score = data[0]
 
     for i in range(len(data) - 1):
-        for j in range(len(data)):
+        for j in range(i+1, len(data)):
             current = data[i]["cos_sim"]
             next = data[j]["cos_sim"]
-            if current > next:
-                temp = current
-                current = next
-                next = temp
+            if current < next:
+                temp = data[i]
+                data[i] = data[j]
+                data[j] = temp
 
     for point in data:
         print(point["cos_sim"])
+
+    with open("./data/pairs_filtered.json", "w") as writer:
+        for point in data:
+            writer.write(json.dumps({
+                "score": point["cos_sim"], 
+                "Person1": point["1"]["content"], 
+                "Person2": point["2"]["content"]
+                }, indent=4))
+
                 
 
 
