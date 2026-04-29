7068717c1df92f914647b14f205813e5194dc972
Rod C <cayabyabrr@yahoo.com>
2026-04-28 21:32:51 -0700

    Filter by conversation_turns

---
+++ segment_cleanse.py
@@ -0,0 +1,21 @@
+import jsonlines
+
+input_segments = "./data/pairs_grouped_topics.jsonl"
+output = "./data/pairs_grouped_topics_filtered.jsonl"
+
+def main():
+    
+    data = []
+    
+    with jsonlines.open(input_segments, "r") as reader, \
+    jsonlines.open(output, "w") as writer:
+        for i, point in enumerate(reader):
+            if i > 1317:
+                break
+            print(point)
+            conversation_turns = len(point["messages"])
+            print(conversation_turns)
+            if conversation_turns <= 3:
+                writer.write(point)
+            
+main()
